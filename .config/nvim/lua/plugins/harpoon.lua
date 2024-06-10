ded41dfe8b1b19527dfab9c3a07f191cb0005021
Josh Hendershot <hendershotjoshr@gmail.com>
2024-06-10 15:55:18 -0600

    fix: moved plenary to be harpoon dependency

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,5 +1,8 @@
 return {
 	'ThePrimeagen/harpoon',
+	dependencies = {
+		'nvim-lua/plenary.nvim'
+	},
 	keys = {
 		{ '<leader>a', function() require('harpoon.mark').add_file() end },
 		{ '<C-e>', function() require('harpoon.ui').toggle_quick_menu() end },

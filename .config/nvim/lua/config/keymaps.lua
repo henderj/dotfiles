d7a499de64a59c4ecdcdeb2599517b89489770bc
Josh Hendershot <hendershotjoshr@gmail.com>
2024-06-12 16:18:21 -0600

    feat: keybindings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,14 @@ map("i", "jk", "<Esc>")
 map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+
+map("v", "J", ":m '>+1<cr>gv=gv")
+map("v", "K", ":m '<-2<cr>gv=gv")
+
+map("n", "J", "mzJ`z")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- map("x", "<leader>p", "\"*p") -- change
+
+map({"n", "v"}, "<leader>y", [["+y]])

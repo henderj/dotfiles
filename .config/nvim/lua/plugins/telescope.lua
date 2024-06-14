07bf7eb97477abca3484e80c37a5b96c67d2d6fe
Josh Hendershot <hendershotjoshr@gmail.com>
2024-06-14 16:54:42 -0600

    fix: load on Telescope command

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,11 +2,12 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   requires = { 'nvim-lua/plenary.nvim' },
+  cmd = 'Telescope',
   keys = {
     { '<leader>pf', function() require('telescope.builtin').find_files() end },
     { '<C-p>', function() require('telescope.builtin').git_files() end },
-    { '<leader>ps', function() 
-      require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") }) 
+    { '<leader>ps', function()
+      require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") })
     end },
   },
 }

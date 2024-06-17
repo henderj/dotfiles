ce1597c071f43d154b8036361d3adc785703ee92
Josh Hendershot <hendershotjoshr@gmail.com>
2024-06-17 10:49:07 -0600

    feat: <leader>gp = :G push

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
   'tpope/vim-fugitive',
   cmd = { 'G', 'Git' },
   keys = {
-    { "<leader>gs", vim.cmd.Git }
+    { "<leader>gs", vim.cmd.Git },
+    { "<leader>gp", "<cmd>G push<cr>" }
   }
 }

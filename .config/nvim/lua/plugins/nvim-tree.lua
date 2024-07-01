de96d223e7a4d12161f858a19bb87fa7633082a2
Josh Hendershot <hendershotjoshr@gmail.com>
2024-07-01 15:05:33 -0600

    feat(nvim/nvim-tree): better config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -9,5 +9,22 @@ return {
     { '<leader>ef', function() require('nvim-tree.api').tree.focus({ find_file = true }) end, desc = "Focus nvim-tree" },
     { '<leader>er',  '<cmd>NvimTreeRefresh<cr>', desc = "Refresh nvim-tree" },
   },
-  config = true
+  config = function()
+    require("nvim-tree").setup {
+      sync_root_with_cwd = true,
+      renderer = {
+        icons = {
+          git_placement = "after"
+        }
+      },
+      diagnostics = {
+        enable = true
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true
+        }
+      }
+    }
+  end
 }

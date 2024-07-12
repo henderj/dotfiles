7430530c74d3d3747c754be59a1a527cd31cf7fd
Josh Hendershot <hendershotjoshr@gmail.com>
2024-07-12 14:02:56 -0600

    feat(nvim): better comments

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,12 @@
 return {
-  'numToStr/Comment.nvim',
-  config = true,
-  enabled = false, -- disable for now. I might use it later though
+  "folke/ts-comments.nvim",
+  opts = {},
+  event = { 'BufReadPre', 'BufNewFile' },
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
 }
+
+-- return {
+--   'numToStr/Comment.nvim',
+--   config = true,
+--   enabled = false, -- disable for now. I might use it later though
+-- }

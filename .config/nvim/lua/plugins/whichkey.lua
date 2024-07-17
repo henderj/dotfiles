2efbe77eb9362d77f3cd6869948999036104bc5a
Josh Hendershot <hendershotjoshr@gmail.com>
2024-07-17 09:31:48 -0600

    feat(nvim/whick-key): add groups and icons

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,11 +1,34 @@
 return {
   "folke/which-key.nvim",
+  -- enabled = false,
   event = "VeryLazy",
-  opts = {
-    modes = {
-      x = false,
-    }
-  },
+  config = function()
+    local wk = require("which-key")
+    wk.setup({
+      modes = {
+        defer = {
+          v = true,
+        },
+      },
+      expand = 1,
+    })
+
+    wk.add({
+      { "<leader>g", group = "git", icon = "󰊢" },
+      { "<leader>e", group = "nvim-tree", icon = "" },
+      { "<leader>L", desc = "Lazy", icon = "󰒲 " },
+      { "<leader>s", desc = "Replace word" },
+      { "<leader>j", group = "Neotest", icon = "󰙨" },
+      { "<leader>t", group = "Telescope" },
+      { "<leader>m", group = "Markdown", icon = "" },
+      { "<leader>x", group = "Trouble", icon = "󰔫" },
+      { "<leader>c", group = "Code", icon = "" },
+      { "<leader>y", desc = "yank to clipboard" },
+      { "<leader>Y", desc = "Yank to clipboard" },
+      { "<leader>p", desc = "put from clipboard" },
+      { "<leader>P", desc = "Put from clipboard" },
+    })
+  end,
   keys = {
     {
       "<leader>?",

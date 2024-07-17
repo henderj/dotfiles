cdf543f725179840f4e818dc373cd578e5225196
Josh Hendershot <hendershotjoshr@gmail.com>
2024-07-17 12:55:45 -0600

    fix(nvim): remove nvim-tree <C-e> keybind in favor of harpoon's

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -24,7 +24,18 @@ return {
         open_file = {
           quit_on_open = true
         }
-      }
+      },
+      on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+
+        vim.keymap.del('n', '<C-e>', { buffer = bufnr })
+      end,
     }
   end
 }

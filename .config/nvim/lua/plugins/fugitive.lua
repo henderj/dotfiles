b3b9066468dd0723a185eea2f9cc566925445992
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-19 15:50:41 -0600

    feat(nvim/fugitive): add git pull and fetch keymaps

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,8 @@ return {
   cmd = { 'G', 'Git' },
   keys = {
     { "<leader>gs", vim.cmd.Git, desc = "Git status" },
-    { "<leader>gp", "<cmd>G push<cr>", desc = "Git push" }
+    { "<leader>gp", "<cmd>G push<cr>", desc = "Git push" },
+    { "<leader>gu", "<cmd>G pull<cr>", desc = "Git pull" },
+    { "<leader>gf", "<cmd>G fetch<cr>", desc = "Git fetch" },
   }
 }

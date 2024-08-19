acd74b94d923e9f06430ad9bd772f08b6a31b363
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-19 13:42:25 -0600

    feat(nvim/dap): add dap plugins

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    'mfussenegger/nvim-dap',
+    keys = {
+      { '<F5>', function() require('dap').continue() end, desc = 'Continue (DAP)'},
+      { '<F10>', function() require('dap').step_over() end, desc = 'Step over (DAP)'},
+      { '<F11>', function() require('dap').step_into() end, desc = 'Step into (DAP)'},
+      { '<F12>', function() require('dap').step_out() end, desc = 'Step out (DAP)'},
+      { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle breakpoint (DAP)'},
+      { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Set conditional breakpoint (DAP)'},
+    },
+    config = function()
+      local js_based_languages = { "javascript", "typescript" }
+
+      for _, language in ipairs(js_based_languages) do
+        require('dap').configurations[language] = {
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch file',
+            program = '${file}',
+            cwd = '${workspaceFolder}'
+          },
+          {
+            type = 'pwa-node',
+            request = 'attach',
+            name = 'Attach',
+            processId = require('dap.utils').pick_process,
+            cwd = '${workspaceFolder}'
+          }
+        }
+      end
+    end
+  },
+  {
+    'mxsdev/nvim-dap-vscode-js',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      {
+        'microsoft/vscode-js-debug',
+        lazy = true,
+        build = 'npm install --legacy-peer-deps && git restore . && npx gulp vsDebugServerBundle && mv dist out'
+      }
+    },
+    opts = {
+      debugger_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug',
+      adapters = { 'pwa-node', 'node-terminal', 'node' },
+    }
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    keys = {
+      { '<leader>du', function() require('dapui').toggle() end, desc = 'Toggle UI (DAP)'}
+    },
+    config = function()
+      require('dapui').setup()
+
+      local dap, dapui = require('dap'), require('dapui')
+
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close({})
+      end
+    end
+  }
+}

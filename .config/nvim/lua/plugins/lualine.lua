716839617c942aa606305bb9ec0c9c1b7c9d6bf3
Josh Hendershot <hendershotjoshr@gmail.com>
2024-08-19 16:06:09 -0600

    feat(nvim/lualine): add lazy package updates to statusline

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,29 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    sections = {
-      lualine_c = {
-        {
-          'filename',
-          path = 1,
+  config = function()
+    local lualine = require('lualine')
+    local lazy_status = require('lazy.status')
+
+    lualine.setup({
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+        },
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = "#ff9e64" }
+          },
+          { "encoding" },
+          { "fileformat" },
+          { "filetype" },
         },
       },
-    },
-  }
+    })
+  end,
 }

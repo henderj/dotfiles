6ffeb44d4a4b8eb7f12e1fd1623794501dd6d571
Josh Hendershot <hendershotjoshr@gmail.com>
2024-10-01 16:57:02 -0600

    feat(nvim): added todo highlighting plugin

---
+++ .config/nvim/lua/plugins/todo.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+  }
+}
+
+-- PERF: fully optimized
+-- HACK: hmmm, this looks a bit funky
+-- TODO: what else?
+-- NOTE: adding a note
+-- FIX: this needs fixing
+-- WARN: ???
+-- TEST: test something

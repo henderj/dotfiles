f48e5774913d65c5a0b8689c6f9d896e6d0fae3a
Josh Hendershot <hendershotjoshr@gmail.com>
2024-10-04 12:26:03 -0600

    feat(nvim): add keymaps to todo plugin

--- .config/nvim/lua/plugins/todo.lua
+++ .config/nvim/lua/plugins/todo.lua
@@ -1,7 +1,11 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
+  events = { "BufReadPre", "BufNewFile" },
+  opts = {},
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" } },
+    { "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" } },
   }
 }
 

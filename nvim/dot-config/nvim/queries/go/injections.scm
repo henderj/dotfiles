464dc176d42d12008f8b55e4b81f4ea0800f31b2
Josh Hendershot <hendershotjoshr@gmail.com>
2025-03-04 09:20:36 -0700

    feat(nvim): add regex injections for go files

--- nvim/dot-config/nvim/queries/go/injections.scm
+++ nvim/dot-config/nvim/queries/go/injections.scm
@@ -1,12 +1,22 @@
 (short_var_declaration
   left: (expression_list
-    (identifier) @_id (#match? @_id "query|sql")
+    (identifier) @_id (#match? @_id "[Qq]uery|[Ss]ql")
   )
   right: (expression_list
     (raw_string_literal
       (raw_string_literal_content) @injection.content
       (#set! injection.language "sql")
-      (#set! injection.combined)
+    )
+  )
+)
+(short_var_declaration
+  left: (expression_list
+    (identifier) @_id (#match? @_id "[Rr]egex")
+  )
+  right: (expression_list
+    (raw_string_literal
+      (raw_string_literal_content) @injection.content
+      (#set! injection.language "regex")
     )
   )
 )

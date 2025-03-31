01a369f7e413257d099bdd9d8076215a6580f658
Josh Hendershot <hendershotjoshr@gmail.com>
2025-03-31 13:38:08 -0600

    feat(nvim): remove js and ts debugger adapters

--- nvim/dot-config/nvim/lua/plugins/dap.lua
+++ nvim/dot-config/nvim/lua/plugins/dap.lua
@@ -9,56 +9,13 @@ return {
             { '<leader>db', function() require('dap').toggle_breakpoint() end,                                    desc = 'Toggle breakpoint (DAP)' },
             { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Set conditional breakpoint (DAP)' },
         },
-        config = function()
-            local dap = require('dap')
-            local js_based_languages = { "javascript", "typescript" }
-
-            for _, language in ipairs(js_based_languages) do
-                dap.configurations[language] = {
-                    {
-                        type = 'pwa-node',
-                        request = 'launch',
-                        name = 'Launch file',
-                        program = '${file}',
-                        cwd = '${workspaceFolder}'
-                    },
-                    {
-                        type = 'pwa-node',
-                        request = 'attach',
-                        name = 'Attach',
-                        processId = require('dap.utils').pick_process,
-                        cwd = '${workspaceFolder}'
-                    }
-                }
-            end
-
-            dap.configurations.go = {
-                {
-                    type = 'delve',
-                    request = "launch",
-                    name = 'Launch File',
-                }
-            }
-        end
-    },
-    {
-        'mxsdev/nvim-dap-vscode-js',
-        dependencies = {
-            'mfussenegger/nvim-dap',
-            {
-                'microsoft/vscode-js-debug',
-                lazy = true,
-                build =
-                'rm -rf out && npm install --legacy-peer-deps && git restore . && npx gulp vsDebugServerBundle && mv dist out'
-            }
-        },
-        opts = {
-            debugger_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug',
-            adapters = { 'pwa-node', 'node-terminal', 'node' },
-        }
+        init = function()
+            vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
+        end,
     },
     {
         'leoluz/nvim-dap-go',
+        ft = 'go',
         config = true,
     },
     {
@@ -79,8 +36,6 @@ return {
         },
         config = function()
             require('dapui').setup()
-            vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
-
             local dap, dapui = require('dap'), require('dapui')
 
             dap.listeners.after.event_initialized['dapui_config'] = function()

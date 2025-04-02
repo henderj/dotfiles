95617cf811d546f1239a2a7425e30495fd875517
Josh Hendershot <hendershotjoshr@gmail.com>
2025-04-02 15:07:17 -0600

    feat(nvim): don't illuminate for html and nvimtree buffers

--- nvim/dot-config/nvim/lua/plugins/illuminate.lua
+++ nvim/dot-config/nvim/lua/plugins/illuminate.lua
@@ -1,4 +1,16 @@
 return {
     'RRethy/vim-illuminate',
     event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+        require('illuminate').configure({
+            filetypes_denylist = {
+                'dirbuf',
+                'dirvish',
+                'fugitive',
+                'html',
+                'gotmpl',
+                'NvimTree'
+            }
+        })
+    end
 }
